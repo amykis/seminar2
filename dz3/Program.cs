dfdf0743e6198e79d08a2ea860f6bfc163d6d602
Aleksey Myakinin <amykis@mail.ru>
2023-12-29 10:52:30 +0300

    add num = 40

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = 40;
+if (num > 10 && num < 99) {
+    int first_digit = num % 10;
+    int second_digit = num / 10;
+    if (first_digit >  second_digit) {
+        Console.Write(first_digit);
+    }
+    else {
+        Console.Write(second_digit);
+    }
+}
