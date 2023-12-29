32c01f261da96e9adf62110af321d54fc8689741
Aleksey Myakinin <amykis@mail.ru>
2023-12-29 15:14:32 +0300

    add n = 568

--- dz4/Program.cs
+++ dz4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+int n = 568;
+int num = n;
+int pow = 1;
+
+while (n > 9) {
+    while (num > 9) {
+    num /= 10;
+    pow = pow * 10;
+    }   
+    Console.Write (num + ",");
+    n = n % (pow);
+    num = n;
+    pow = 1;
+    }
+Console.Write (n);
+
+
+
+
+
